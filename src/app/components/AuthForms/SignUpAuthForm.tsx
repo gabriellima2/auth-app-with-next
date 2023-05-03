d294d5c46a5d9afe01354df08db060a49ea446cd
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-03 15:17:54 -0300

    feat: add fields to signup-auth-form

--- src/app/components/AuthForms/SignUpAuthForm.tsx
+++ src/app/components/AuthForms/SignUpAuthForm.tsx
@@ -4,12 +4,24 @@ import { useForm } from "react-hook-form";
 import { BaseForm, Field } from "../common";
 
 export const SignUpAuthForm = () => {
-	const { register, handleSubmit } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 	return (
 		<BaseForm
 			handleSubmit={handleSubmit((data) => console.log(data))}
 			button={{ text: "Criar", title: "Criar conta" }}
 		>
+			<Field
+				labelText="Nome de usuário"
+				id="username"
+				type="text"
+				placeholder="Ex: anonymous123"
+				required
+				{...register("username")}
+			/>
 			<Field
 				labelText="Email"
 				id="email"
@@ -17,6 +29,14 @@ export const SignUpAuthForm = () => {
 				placeholder="seuemail@gmail.com"
 				required
 				{...register("email")}
+			/>
+			<Field
+				labelText="Senha"
+				id="password"
+				type="password"
+				placeholder="Senha com 8 ou mais caracteres"
+				required
+				{...register("password")}
 			/>
 		</BaseForm>
 	);
