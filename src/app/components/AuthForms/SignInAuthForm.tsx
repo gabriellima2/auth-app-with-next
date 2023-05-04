aff2b40712662326d70e1d883775ed878632e99c
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-04 14:53:49 -0300

    feat: adds signin-auth-form component

--- src/app/components/AuthForms/SignInAuthForm.tsx
+++ src/app/components/AuthForms/SignInAuthForm.tsx
@@ -1,3 +1,35 @@
+"use client";
+import { useForm } from "react-hook-form";
+
+import { BaseForm, Field } from "../common";
+
 export const SignInAuthForm = () => {
-	return <form></form>;
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	return (
+		<BaseForm
+			handleSubmit={handleSubmit((data) => console.log(data))}
+			button={{ text: "Entrar", title: "Entrar na sua conta" }}
+		>
+			<Field
+				labelText="Email"
+				id="email"
+				type="email"
+				placeholder="seuemail@gmail.com"
+				required
+				{...register("email")}
+			/>
+			<Field
+				labelText="Senha"
+				id="password"
+				type="password"
+				placeholder="Senha com 8 ou mais caracteres"
+				required
+				{...register("password")}
+			/>
+		</BaseForm>
+	);
 };
