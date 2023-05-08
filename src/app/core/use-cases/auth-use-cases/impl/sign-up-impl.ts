2d0292a0f20420bb9a96913d54f098d809e159b9
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-08 14:33:19 -0300

    feat: adds initial implementation to sign-up use case

--- src/app/core/use-cases/auth-use-cases/impl/sign-up-impl.ts
+++ src/app/core/use-cases/auth-use-cases/impl/sign-up-impl.ts
@@ -1,7 +1,16 @@
+import { IAuthRepository } from "@/app/core/repositories/auth-repository/auth-repository";
+
 import { UserDTOInput, UserSignUpDTOOutput } from "@/app/core/dtos/user-dtos";
+import { DefaultError } from "@/app/core/errors/default-error";
 import { ISignUp } from "../sign-up";
 
 export class SignUpImpl implements ISignUp {
+	constructor(
+		private readonly authRepository: IAuthRepository,
+		private readonly encryptPassword: (password: string) => Promise<string>,
+		private readonly validate: (user: UserDTOInput) => DefaultError
+	) {}
+
 	async execute(user: UserDTOInput): Promise<UserSignUpDTOOutput> {
 		return {} as UserSignUpDTOOutput;
 	}
