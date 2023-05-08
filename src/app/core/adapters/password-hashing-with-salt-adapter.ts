60e9d5957906ea167ca2d2d84ad30524f11e4f9f
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-08 16:58:47 -0300

    refactor: remove external code dependencies on adapter implementation password-hashing-with-salt-adapter

--- src/app/core/adapters/password-hashing-with-salt-adapter.ts
+++ src/app/core/adapters/password-hashing-with-salt-adapter.ts
@@ -1,12 +1,13 @@
 import bcrypt from "bcrypt";
 
 import { EncryptedPasswordDTO } from "../dtos/encrypted-password-dto";
-import { generateSalt } from "../helpers/generate-salt";
 
 export class PasswordHashingWithSaltAdapterIml {
+	constructor(private readonly generateSalt: () => string) {}
+
 	hash(password: string): EncryptedPasswordDTO | null {
 		let passwordHash: undefined | string;
-		const salt = generateSalt();
+		const salt = this.generateSalt();
 		bcrypt.hash(password, salt, (err, hash) => {
 			if (err) return;
 			passwordHash = hash;
