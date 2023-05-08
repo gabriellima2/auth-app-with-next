b602e18a46e5776e31bd09ad8e21854b829383a5
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-08 16:40:45 -0300

    feat: adds password encryption to the implementation of the sign-up use case

--- src/app/core/use-cases/auth-use-cases/impl/sign-up-impl.ts
+++ src/app/core/use-cases/auth-use-cases/impl/sign-up-impl.ts
@@ -1,17 +1,28 @@
-import { IAuthRepository } from "@/app/core/repositories/auth-repository/auth-repository";
-
-import { UserDTOInput, UserSignUpDTOOutput } from "@/app/core/dtos/user-dtos";
-import { DefaultError } from "@/app/core/errors/default-error";
 import { ISignUp } from "../sign-up";
+
+import { EncryptedPasswordDTO } from "@/app/core/dtos/encrypted-password-dto";
+import { UserDTOInput, UserSignUpDTOOutput } from "@/app/core/dtos/user-dtos";
+
+import { DefaultError } from "@/app/core/errors/default-error";
+import { IAuthRepository } from "@/app/core/repositories/auth-repository/auth-repository";
 
 export class SignUpImpl implements ISignUp {
 	constructor(
 		private readonly authRepository: IAuthRepository,
-		private readonly encryptPassword: (password: string) => Promise<string>,
-		private readonly validate: (user: UserDTOInput) => DefaultError
+		private readonly encryptPassword: (
+			password: string
+		) => EncryptedPasswordDTO | null,
+		private readonly validateCredentials: (user: UserDTOInput) => DefaultError
 	) {}
 
 	async execute(user: UserDTOInput): Promise<UserSignUpDTOOutput> {
+		const { message } = this.validateCredentials(user);
+		if (message) throw new Error(message);
+
+		const encryptedPassword = this.encryptPassword(user.password);
+		if (!encryptedPassword)
+			throw new Error("An error occurred while encrypting the password");
+
 		return {} as UserSignUpDTOOutput;
 	}
 }
