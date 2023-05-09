be4ca6bc2b7123c1adad2910a05b6fc4b43497a2
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-09 14:27:05 -0300

    feat: adds better error handling

--- src/app/core/errors/default-error.ts
+++
@@ -1,3 +0,0 @@
-export interface DefaultError {
-	message: string | undefined | null;
-}
--- src/app/core/use-cases/user-use-cases/impl/sign-up-impl.ts
+++ src/app/core/use-cases/user-use-cases/impl/sign-up-impl.ts
@@ -1,8 +1,10 @@
 import { ISignUp } from "../sign-up";
 
 import { UserDTOInput, UserSignUpDTOOutput } from "@/app/core/dtos/user-dtos";
+import { APIError } from "@/app/core/errors/api-error";
 
 import { ValidationError } from "@/app/core/errors/validation-error";
+import { HttpStatusCode } from "@/app/core/helpers/http-status-code";
 import { IUserRepository } from "@/app/core/repositories/user-repository/user-repository";
 
 export class SignUpImpl implements ISignUp {
@@ -16,11 +18,14 @@ export class SignUpImpl implements ISignUp {
 
 	async execute(user: UserDTOInput): Promise<UserSignUpDTOOutput> {
 		const { message } = this.validateCredentials(user);
-		if (message) throw new Error(message);
+		if (message) throw new APIError(message, HttpStatusCode.unauthorized);
 
 		const encryptedPassword = this.encryptPassword(user.password);
 		if (!encryptedPassword)
-			throw new Error("An error occurred while encrypting the password");
+			throw new APIError(
+				"An error occurred while encrypting the password",
+				HttpStatusCode.serverError
+			);
 
 		return {} as UserSignUpDTOOutput;
 	}
