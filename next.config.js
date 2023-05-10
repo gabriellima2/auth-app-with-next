0b8920733d63d099c53bed86a026d1b38c474940
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-05-10 17:34:54 -0300

    chore: removes unnecessary configs

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-	webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
